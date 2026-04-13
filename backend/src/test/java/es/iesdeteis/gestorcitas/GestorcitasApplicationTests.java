c33c45ffcd3c358de805951e886ac150804ba0ec
Ruben Dario Anzola Sánchez <a24rubendas@teis.intranet>
2026-04-13 18:45:22 +0200

    init spring boot

---
+++ backend/src/test/java/es/iesdeteis/gestorcitas/GestorcitasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesdeteis.gestorcitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorcitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
