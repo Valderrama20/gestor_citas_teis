7b10d22f0e92e22fa15a609a0734e8eac6a3a5f7
Ruben Dario Anzola Sánchez <a24rubendas@teis.intranet>
2026-04-20 17:53:37 +0200

    folder reorganization

--- backend/src/main/java/es/iesdeteis/gestorcitas/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package es.iesdeteis.gestorcitas.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/es/iesdeteis/gestorcitas/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package es.iesdeteis.gestorcitas.util;
-
-public class Constants {
-    public static final String API_BASE_URL = "/api/v1";
-}
