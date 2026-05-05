912a013142f458607647d654785b5e9ecb0da1ce
ruben anzola <anzola.ruben@gmail.com>
2026-05-05 23:14:46 +0200

    fix: Fix clientid attribute name in model for persistence

--- backend/src/main/java/es/iesdeteis/gestorcitas/controller/CitaController.java
+++ backend/src/main/java/es/iesdeteis/gestorcitas/controller/CitaController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/citas")
+@CrossOrigin(origins = "*")
 public class CitaController {
 
     // --- ATRIBUTOS ---
--- backend/src/main/java/es/iesdeteis/gestorcitas/controller/ClienteController.java
+++ backend/src/main/java/es/iesdeteis/gestorcitas/controller/ClienteController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
